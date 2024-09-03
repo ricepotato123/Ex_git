2cf157398cf9b1dddae86bd6603ac82cfdaa096d
ramak <ramak@DESKTOP-1MGB1ER>
2024-09-03 09:47:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_git
+{
+    public class Program
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(1, 2));
+            Console.WriteLine(Sub(5, 3));
+            Console.ReadLine();
+
+        }
+    }
+}
